6a8bcfbc7390c65cc39faadb51ff327d620768d9
kuntalrpr <59866113+kuntalrpr@users.noreply.github.com>
2020-01-14 14:41:50 +0530

    Add files via upload

---
+++ bilab2.pl
@@ -0,0 +1,9 @@
+#!/include/bin/perl;
+$DNA1='AGCTCATCG'
+print "1st DNA Sequence:$DNA1\n";
+$DNA2='CAGATCTAATC';
+print "2nd DNA Sequence:$DNA2\n";
+$DNAcon1=$DNA2$DNA2; #string intetrpolation
+$DNAcon2=$DNA2.$DNA2; #dot intetrpolation
+$DNAcon3=$DNA2,$DNA2; # intetrpolation
+exit;
